e3ba84ae9f1699af5738a233c8d1e0d1da16328d
create-stree <kirsiacb@gmail.com>
2026-05-28 15:51:53 +0900

    Update loader.js

--- api/loader.js
+++ api/loader.js
@@ -1,8 +1,8 @@
 export default function handler(req, res) {
+  res.setHeader("Content-Type", "text/plain")
+  res.setHeader("Access-Control-Allow-Origin", "*")
 
-    res.setHeader("Content-Type", "text/plain")
-
-    res.send(`
+  res.send(`
 loadstring(game:HttpGet("https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Loader/Main.lua", true))()
 `)
 }
